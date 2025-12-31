76ee31c999761726bf5cb0dea75cf4b304fa09fe
pirate-jpg <2s5sr4b9bj@privaterelay.appleid.com>
2025-12-30 20:19:54 -0600

    Set up minimal Express server with Socket.io

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+// Pirate Cribbage - minimal server (works on Railway)
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(express.static("public"));
+
+app.get("/health", (req, res) => res.send("ok"));
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  socket.emit("hello", { msg: "🏴‍☠️ Ahoy! Pirate Cribbage server is alive." });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log("Listening on", PORT));
 
